fd545f085cc2bfb7d480e1cfd71511ce273e2890
Du Shiqiao <lucidfrontier.45@gmail.com>
2023-02-13 12:11:17 +0900

    generalized logistic  annealing to relative annealing

--- src/tests/test_relative_annealing.rs
+++ src/tests/test_relative_annealing.rs
@@ -1,13 +1,14 @@
 use approx::assert_abs_diff_eq;
 
-use crate::optim::LogisticAnnealingOptimizer;
+use crate::optim::{relative_transition_score, RelativeAnnealingOptimizer};
 
 use super::QuadraticModel;
 
 #[test]
 fn test() {
     let model = QuadraticModel::new(3, vec![2.0, 0.0, -3.5], (-10.0, 10.0));
-    let opt = LogisticAnnealingOptimizer::new(5000, 10, 1e1);
+    let opt =
+        RelativeAnnealingOptimizer::new(5000, 10, |ds| relative_transition_score::exp(ds, 1e1));
     let null_closure = None::<&fn(_)>;
     let (final_state, final_score) = opt.optimize(&model, None, 10000, null_closure);
     assert_abs_diff_eq!(2.0, final_state[0], epsilon = 0.05);
